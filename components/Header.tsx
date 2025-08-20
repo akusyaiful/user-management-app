37e270a2fd22cca26fb27f3264ba4d1904c9ff0d
akusyaiful <msyaifullah13@gmail.com>
2025-08-21 03:15:46 +0700

    addjust padding header

--- components/Header.tsx
+++ components/Header.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Header = () => {
   return (
-    <div className="w-full py-4 px-8 shadow-md bg-white">
+    <div className="w-full py-6 px-8 shadow-md bg-white">
       <p className="font-bold">User Management App</p>
     </div>
   );
