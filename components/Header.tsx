fd307da0a2ad8b1de99ad5e61afddc8000895fd1
akusyaiful <msyaifullah13@gmail.com>
2025-08-21 03:17:48 +0700

    add logo on header

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,8 +1,10 @@
+import { CircleUser } from "lucide-react";
 import React from "react";
 
 const Header = () => {
   return (
-    <div className="w-full py-6 px-8 shadow-md bg-white">
+    <div className="flex items-center gap-4 w-full py-6 px-8 shadow-md bg-white">
+      <CircleUser className="w-8 h-8" />
       <p className="font-bold">User Management App</p>
     </div>
   );
